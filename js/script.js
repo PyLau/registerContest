22b03088926565eb8f2a71f89d54ea6d602aa0f0
Laura Parra <lauraparra89@gmail.com>
2016-09-11 19:37:35 -0400

    ajax call

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 angular
-	.module("myApp", [])
+	.module("myApp", ['ngMockE2E'])
 
-	.controller("RegisterCtrl", function ($scope) {
+	.run(function($httpBackend){
+		$httpBackend.whenPOST('/valid').respond(function(method, url, data) {
+			return [200, {'valid':false}, {}];
+		});
+	})
+
+	.controller("RegisterCtrl", function ($scope, $http) {
 		$scope.registerf = function(){
-			console.log($scope.user);
-		}
+			$http.post('/valid', $scope.user).then(function(response){
+				if (response.data.valid){
+					console.log(response.data.valid);
+				}
+			});
+		};
 	})
 ;
