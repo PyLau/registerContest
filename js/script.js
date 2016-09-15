47f65b7fdd05194d234f307c07ddee0336fdf5f6
Laura Parra <lauraparra89@gmail.com>
2016-09-14 23:21:27 -0400

    fixing

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ angular
 
 	.run(function($httpBackend){
 
+		var validTrue = {"status":true, "user":{"id":41, "name":"Wiljac", "lastName":"Aular", "cedula":"19960100", "email":"waular@eluniversal.com", "phone":"04129350470"}, "answers":[{"pubDate":"13/09/2016", "answer":"Respuesta 1", "position":"1", "status":true }, {"pubDate":"14/09/2016", "answer":"Respuesta 2", "position":"2", "status":true },{"pubDate":"15/09/2016", "position":"3", "status":false },{"pubDate":"16/09/2016", "position":"4", "status":false }, {"pubDate":"17/09/2016", "position":"5", "status":false },{"pubDate":"18/09/2016", "position":"6", "status":false },{"pubDate":"19/09/2016", "position":"7", "status":false }, {"pubDate":"20/09/2016", "position":"8", "status":false },{"pubDate":"21/09/2016", "position":"9", "status":false },{"pubDate":"22/09/2016", "position":"10", "status":false } ] };
+
 		$httpBackend.whenGET(new RegExp('\\/validservice\\/[0-9]+')).respond(function(method, url, data) {
-			return [200, {'status':true}, {}];
+			return [200, validTrue, {}];
 		});
 		$httpBackend.whenPOST('/register').respond(function(method, url, data) {
 			return [200, {'answers': {'a1': '', 'a2': '', 'a3': '', 'a4' : ''}}, {}];
@@ -22,43 +24,33 @@ angular
 				console.log(response.data.status);
 				if (!response.data.status){ //si es falso el usuario no esta registrado
 					$scope.showRegister = true; // muestro el registro
-					$(".form-login").hide(); //escondo el login
-					var data = $.param({ // guardo la data del usuario que ingresa en el formulario
-		            	name: $scope.user.first_name,
-		            	lastName: $scope.user.last_name,
-		            	ci: $scope.user.ci,
-		            	email: $scope.user.mail,
-		            	phone: $scope.user.phone
-		           	});
-					$http.post('/register', data).then(function(response){ //envio los datos del usuario al servicio que lo registra y me devuelve el id y las respuestas vacias 
-						console.log(response);
-					});
+					// $(".form-login").hide(); //escondo el login
+					// var data = $.param({ // guardo la data del usuario que ingresa en el formulario
+		   //          	name: $scope.user.first_name,
+		   //          	lastName: $scope.user.last_name,
+		   //          	ci: $scope.user.ci,
+		   //          	email: $scope.user.mail,
+		   //          	phone: $scope.user.phone
+		   //         	});
+					// $http.post('/register', data).then(function(response){ //envio los datos del usuario al servicio que lo registra y me devuelve el id y las respuestas vacias 
+					// 	console.log(response);
+					// });
 				}else{ // si es verdadero el usuario esta registrado
 					$(".form-login").hide(); //se esconde el login
-					registerFunction();
+					console.log(response.data.answers);
+					$scope.showAnswers = true;
+					$scope.answers = response.data.answers;
 				}
 			});
 		};
-		$scope.registerf = function(){
-			$(".form-register").hide();
-			registerFunction();
-		};
+		// $scope.registerf = function(){
 
-		function registerFunction(){
-			var data = $.param({
-            	name: $scope.user.first_name,
-            	lastName: $scope.user.last_name,
-            	ci: $scope.user.ci,
-            	email: $scope.user.mail,
-            	phone: $scope.user.phone
-           	});
-			$http.post('/answers', data).then(function(response){
-				console.log(data);
-				$scope.showAnswers = true;
-				console.log(response);
-				$scope.user.a1 = response.data.answers.a1;
-			});
-		}
+		// }
+		// $scope.sendAnswers = function(){
+
+		// }
+
+
 	})
 
 
