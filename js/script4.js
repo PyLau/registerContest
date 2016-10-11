2b2eb916c7f07cafd0725b1cefa2db2fde9c5152
Laura Parra <lauraparra89@gmail.com>
2016-10-11 01:24:35 -0400

    answers

--- js/script4.js
+++ js/script4.js
@@ -40,8 +40,15 @@
       "Yaracuy",
       "Zulia"
     ];
-
-
+    $scope.dates = [
+      "14/10/2016",
+      "15/10/2016",
+      "16/10/2016",
+      "17/10/2016",
+      "18/10/2016",
+      "19/10/2016"
+    ];
+    $scope.answers = [];
   $scope.hidebases = function() {
     $scope.slide = false;
   }
@@ -55,14 +62,22 @@
       )
       .success(function (data) {
             console.log(data.status);
-            // $scope.isLoading = true;
             $scope.showLogin = false; //escondo el login
             if (!data.status){ //si es falso el usuario no esta registrado
               $timeout(function() {
                 $scope.showRegister = true;
               }, 500);
-           }else{ // si es verdadero el usuario esta registrado
-              // $scope.isLoading = false;
+           }else{ 
+              angular.forEach(data.user, function(value, key) {
+                console.log(value);
+
+
+              }, log);
+            // si es verdadero el usuario esta registrado
+            //se muestra el formulario de respuestas
+            //si el usuario tiene respuestas cargadas se muestran
+            //
+
              // $scope.showAnswers = true;
              // console.log(data.answers);
              // $scope.answersResp = data.answers;
@@ -74,8 +89,8 @@
       });
    };
    $scope.register = function(){
+      $scope.isLoading = true;
       $scope.showRegister = false; //se esconde el registro
-      // $scope.showAnswers = true; 
       var data = {"name":$scope.user.name,
                   "lastName":$scope.user.lastname,
                   "cedula": $scope.user.ci,
@@ -84,17 +99,30 @@
                   "address": $scope.state
                };
       console.log(data); //a continuacion se llama al servicio que registra
-      $http.post('http://widget.eluniversal.com/ContestAdmin/services/questions/user/register/281/', data, {headers : { 
-        'Content-Type': 'application/json;charset=utf-8;'
-      }}) //envio la dara completa del usuario al servicio y me devuelve los mismos datos mas el id
-            .success(function (data) {
-               console.log(data);
-               var userid = data.user.id;
-               console.log(userid);
-               $scope.answers = data.answers;
+      $http.post('http://widget.eluniversal.com/ContestAdmin/services/questions/user/register/281/', data, {
+        headers : { 
+          'Content-Type': 'application/json;charset=utf-8;'
+        }}
+      ) //envio la dara completa del usuario al servicio y me devuelve los mismos datos mas el id
+        .success(function (data) {
+          console.log(data);
+          var userid = data.user.id;
+          console.log(userid);
+          // $scope.answers = data.answers;
+          $scope.showAnswers = true; 
+      })
+      .finally(function () {
+        // Hide loading whether our call succeeded or failed.
+        $scope.isLoading = false;
       });
    };
 
+    $scope.sendAnswers = function(){
+      //lamada al servicio que guardara la participacion
+
+      $scope.answers.push($scope.user.answer);
+      console.log($scope.answers);
+    };
 })
 
 
